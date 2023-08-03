b5fdb2b4eba68caeac2dbea96dd89704bb90f81f
LeonardoTalanski <leonardo.e.t@hotmail.com>
2023-08-03 19:33:04 -0300

    Reformulação de código js.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,45 @@
+
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
-
-const jump = () => {
-    mario.classList.add('jump');
-
-    setTimeout(() => {
-         mario.classList.remove('jump');
-         }, 500);
-}
-
+const audioJump = document.querySelector('.audiojump');
+const gameOver = document.querySelector('.gameover');
+const textStart = document.querySelector('#text-start')
 
 const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    console.log('loop')
+    /* condição lógica que define COLISÃO */
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
 
-const pipePosition = pipe.offsetLeft;
-const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
 
-console.log(pipePosition);
-console.log(marioPosition);
+        mario.src = 'images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px'
+      
+        gameOver.currentTime = 0.1;
+        gameOver.volume = 0.2;
+        gameOver.play();
+     
+        document.getElementById("text-start").style.color = "black";
+        document.getElementById("text-start").innerHTML = "<strong>GAME OVER</strong>";
+        clearInterval(loop);
+    }
+}, 10);
 
-if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
-    pipe.style.animation = 'none';
-    pipe.style.left = `${pipePosition}px`;
-
-    mario.style.animation = 'none';
-    mario.style.bottom = `${marioPosition}px`;
-
-    mario.src = './images/game-over.png';
-    mario.style.width = "75px";
-    mario.style.marginLeft = "50px";
-
-    clearInterval(loop);
-}
-
-}, 5);
+const rotinapular = () => {
+    mario.classList.add('jump');
+    audioJump.currentTime = 0.1;
+    audioJump.volume = 0.1;
+    audioJump.play();
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+} 
+document.addEventListener('keydown', rotinapular);
 
 
-document.addEventListener('keydown', jump);
